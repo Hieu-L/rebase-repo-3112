75b7236e98f7c7199d638fa0afa30fbfcc0a6bae
HieuSWO <ext.hieu.le@softwareone.com>
2025-12-31 16:00:54 +0700

    3rd person feature

--- b.py
+++ b.py
@@ -3,3 +3,6 @@
 
 def foo_b(s:str) :
     return s in (s+s)[1:len(s)*2-1]
+
+def foo_b(s:str) :
+    return s in (s+s)[1:len(s)*2-1]
--- c.py
+++ c.py
@@ -5,3 +5,8 @@ def foo_c(n:int) :
 
     for i in range(1,n+1):
         print('*'*i)
+
+
+def foo_a(n: int) :
+    return (n+(n+1))/2
+
